4fc5ad46ec932b0665ea97289dcadef957c2f635
Daniel Quackenbush <TheQuack45@gmail.com>
2016-08-14 13:24:50 -0400

    Switched to an offline synonym search method. Began setting up WordNet integration.

--- SpewspeakActualReal/Model/Conversion.cs
+++ SpewspeakActualReal/Model/Conversion.cs
@@ -31,6 +31,7 @@ namespace SpewspeakActualReal.Model
         #region Methods definition
         public static string ConvertSentence(string sentence)
         {
+            WordNetClasses.WN wordNetIns = new WordNetClasses.WN(@"inc\WordNet\");
             StringBuilder convertedSentenceBuilder = new StringBuilder();
 
             string[] splitSentence = Regex.Split(sentence, WORD_DELIMITER_SIMPLE);
@@ -43,7 +44,7 @@ namespace SpewspeakActualReal.Model
                 }
                 else
                 {
-                    convertedSentenceBuilder.Append(MostComplexSynonym(word));
+                    convertedSentenceBuilder.Append(MostComplexSynonymOffline(word));
                 }
                 convertedSentenceBuilder.Append(WORD_DELIMITER_SIMPLE);
             }
@@ -73,8 +74,42 @@ namespace SpewspeakActualReal.Model
         public static string MostComplexSynonymOffline(string word)
         {
             MyThes thesaurus = new MyThes(Properties.Resources.th_en_US_new);
-            //Hunspell hspell = new Hunspell(Properties.Resources.en_US_aff, Properties.Resources.en_US_dic);
-            
+            ThesResult lookupResult = thesaurus.Lookup(word);
+
+            if (lookupResult == null)
+            {
+                // There are no synonyms for the word given.
+                // Return the standard word.
+                // TODO: THIS IS A CURRENT WORKAROUND METHOD. FUTURE VERSIONS SHOULD TRY TO CHECK IF THIS WORD IS A SPECIFIC TENSE OF A
+                // VERB/ETC AND TRY TO PUT IT INTO A DIFFERENT TENSE AND LOOK THAT UP INSTEAD.
+                return word;
+            }
+
+            Dictionary<string, List<ThesMeaning>> synonymsDict = lookupResult.GetSynonyms();
+            string mostComplexWord = "";
+
+            #region Synonym collection setup
+            List<string> synonyms = new List<string>();
+            foreach (string cword in synonymsDict.Keys)
+            {
+                // Get each key from the results Dictionary and add it to the list of synonyms.
+                synonyms.Add(cword);
+            }
+
+            if (synonyms.Contains(word)) { /* Word is already in the list of synonyms. Do not do anything. */ }
+            else { /* Word is not in the list of synonyms. Add it. */ synonyms.Add(word); }
+            #endregion
+
+            #region Most complex synonym choice
+            foreach (string cw in synonyms)
+            {
+                // Check if the current word is more complex than the previous most complex word.
+                // If so, replace the most complex word with the current word.
+                if (mostComplexWord == "" || WordRater.CompareWords(mostComplexWord, cw) == cw) { mostComplexWord = cw; }
+            }
+            #endregion
+
+            return mostComplexWord;
         }
         #endregion
     }
